493fff800ef3ab88ada7802f3cddaf781b55cd2c
Felix Schmuckermaier <felix.schmuckermaier@tum.de>
2020-03-24 05:26:22 -0500

    creating POCAM module

---
+++ generatePOCAM_Module.py
@@ -0,0 +1,91 @@
+from icecube import icetray, dataclasses
+from I3Tray import I3Units
+from icecube.dataclasses import I3Position
+from icecube.dataclasses import *
+from icecube.clsim import I3CLSimFlasherPulse, I3CLSimFlasherPulseSeries
+from datetime import datetime
+import math
+import random
+
+class GeneratePOCAM_Module(icetray.I3Module):
+    """
+    Generate a POCAM Flash into a DAQ Frame.
+    """
+    def __init__(self, context):
+        icetray.I3Module.__init__(self, context)
+        self.AddParameter("SeriesFrameKey",
+                          "Name of the I3Frame Key the photon flash should be written to",
+                          "PhotonFlasherPulseSeries")
+        self.AddParameter("PhotonPosition",
+                          "The position of the photon source.",
+                          I3Position(0,0,0))
+        self.AddParameter("PhotonDirection",
+                          "The direction of the photon source",
+                          I3Direction(0,0,0))
+        self.AddParameter("NumOfPhotons",
+                          "The number of photons to inject from the given position.",
+                          1)
+        self.AddParameter("FlasherPulseType",
+                          "The I3CLSimFlasherPulse.FlasherPulseType of the photon flashs. For a list, see: https://github.com/claudiok/clsim/blob/master/public/clsim/I3CLSimFlasherPulse.h#L59",
+                          I3CLSimFlasherPulse.FlasherPulseType.LED340nm)
+        self.AddParameter("PlaneWave",
+                          "Whether to start the photons from a plane rather than a point",
+                          False)
+        self.AddParameter("PlaneWaveSize",
+                          "How big should the plane be in metres, e.g. 1.0.",
+                          1.0)
+        self.AddParameter("Seed",
+                          "Seed for the random number generator",
+                          1234)
+        self.AddOutBox("OutBox")
+
+    def Configure(self):
+        self.series_frame_key = self.GetParameter("SeriesFrameKey")
+        self.photon_position = self.GetParameter("PhotonPosition")
+        self.photon_direction = self.GetParameter("PhotonDirection")
+        self.num_of_photons = self.GetParameter("NumOfPhotons")
+        self.pulse_type = self.GetParameter("FlasherPulseType")
+        self.plane_wave = self.GetParameter("PlaneWave")
+        self.plane_wave_size = self.GetParameter("PlaneWaveSize")
+        self.seed = self.GetParameter("Seed")
+
+    def generate_pulse(self, photon_position, photon_direction, number_of_photons):
+        pulse = I3CLSimFlasherPulse()
+        pulse.SetPos(photon_position)
+        pulse.SetDir(photon_direction)
+        pulse.SetTime(0.0*I3Units.ns)
+        pulse.SetNumberOfPhotonsNoBias(number_of_photons)
+        pulse.SetType(self.pulse_type)
+
+        # Pulse duration:
+        pulse.SetPulseWidth(5. * I3Units.ns) #POCAM: ~5ns
+        # Isotropic emission (cover full sphere):
+        pulse.SetAngularEmissionSigmaPolar( 180. * I3Units.deg ) 
+        pulse.SetAngularEmissionSigmaAzimuthal( 360. * I3Units.deg )
+
+        return pulse
+
+    def DAQ(self, frame):
+        random.seed(self.seed)
+
+        pulse_series = I3CLSimFlasherPulseSeries()
+
+        if self.plane_wave:
+            for i in xrange(self.num_of_photons):
+                random_vector = I3Position(random.random() * 2 - 1, random.random() * 2 - 1, random.random() * 2 - 1)
+                shift_vector_a = self.photon_direction.cross(random_vector)
+                shift_vector_b = self.photon_direction.cross(shift_vector_a)
+                len_a = math.sqrt(shift_vector_a * shift_vector_a)
+                len_b = math.sqrt(shift_vector_b * shift_vector_b)
+                new_photon_position = self.photon_position + \
+                    shift_vector_a / len_a * (random.random() * 2 - 1) * self.plane_wave_size / 2 + \
+                    shift_vector_b / len_b * (random.random() * 2 - 1) * self.plane_wave_size / 2
+                pulse = self.generate_pulse(new_photon_position, self.photon_direction, 1)
+                pulse_series.append(pulse)
+        else:
+            pulse = self.generate_pulse(self.photon_position, self.photon_direction, self.num_of_photons)
+            pulse_series.append(pulse)
+
+        frame[self.series_frame_key] = pulse_series
+        self.PushFrame(frame, "OutBox")
+
--- simulate_POCAM_flashes.py
+++ simulate_POCAM_flashes.py
@@ -1,3 +1,104 @@
-import numpy as np
-#Test 
+#!/usr/bin/env python
+
+from optparse import OptionParser
+import glob
+from os.path import expandvars
+
+from icecube import dataclasses
+from icecube.dataclasses import *
+from icecube import icetray, dataio, phys_services, clsim
+from icecube.icetray import I3Tray
+from icecube import simclasses
+from I3Tray import * # otherwise the C++ modules have the wrong signatures
+
+
+### Command line options
+# =============================================================================
+
+parser = OptionParser(usage = "Usage: python propagate_photons_with_clsim.py options infile1.i3 infile2.i3 ...")
+parser.add_option("--output-i3-file", help = "I3 File to write the numbers of dom hits for each run to, e.g. tmp/numbers_of_dom_hits.i3")
+parser.add_option("--output-text-file", help = "File to write the number of dom hits for each run to, one line per run, e.g. tmp/numbers_of_dom_hits.txt", default = "tmp/number_of_dom_1_1_hits.txt")
+parser.add_option("--ice-model", dest = "ice_model_file", help = "e.g. $I3_SRC/clsim/resources/ice/spice_mie")
+parser.add_option("--seed", type = "int", help = "e.g. 123456")
+parser.add_option("--number-of-frames", type = "int", default = 0)
+parser.add_option("--number-of-parallel-runs", type = "int")
+parser.add_option("--save-photon-paths", default = False)
+parser.add_option("--use-flasher-info-vect", default = False)
+parser.add_option("--thinning-factor", type = "float", default = 1.0, help = "Run expensive simulations with less photons. See https://github.com/fiedl/hole-ice-study/issues/85.")
+
+(options, args) = parser.parse_args()
+
+input_files = args
+
+if isinstance(input_files, basestring):
+    input_files = eval(input_files)
+
+om_key = OMKey(1,1)
+
+counter = 0
+numbers_of_dom_hits = []
+def ReadOutAngularAcceptance(frame):
+    global numbers_of_dom_hits
+    n = 0
+    if (frame['MCPESeriesMap'].items() != []):
+        if not frame['MCPESeriesMap'].get(om_key) is None:
+            n = len(frame['MCPESeriesMap'].get(om_key))
+    numbers_of_dom_hits.append(n)
+    global counter
+    counter += 1
+
+tray = I3Tray()
+tray.AddModule("I3Reader",
+               FilenameList = input_files)
+tray.AddService("I3SPRNGRandomServiceFactory",
+                Seed = options.seed,
+                NStreams = 2,
+                StreamNum = 1)
+randomService = phys_services.I3SPRNGRandomService(seed = options.seed,
+                                                   nstreams = 10000,
+                                                   streamnum = 1)
+
+common_clsim_parameters = dict(
+    PhotonSeriesName = "PropagatedPhotons",
+    RandomService = randomService,
+    IceModelLocation = options.ice_model_file,
+    UnWeightedPhotons = True,
+    ParallelEvents = options.number_of_parallel_runs,
+    UnWeightedPhotonsScalingFactor = options.thinning_factor,
+    DOMOversizeFactor = 1.0,
+    UnshadowedFraction = 1.0,
+    FlasherInfoVectName = ("I3FlasherInfo" if options.use_flasher_info_vect else ""),
+    FlasherPulseSeriesName = ("PhotonFlasherPulseSeries" if not options.use_flasher_info_vect else ""),
+)
+
+tray.AddSegment(clsim.I3CLSimMakeHits,
+                StopDetectedPhotons = True,
+                ExtraArgumentsToI3CLSimModule = extra_args,
+                **common_clsim_parameters
+                )
+
+if not options.save_photon_paths:
+    # This does only work with the `I3CLSimMakeHits` module, thus does
+    # not work with `save_photon_paths`.
+    tray.AddModule(ReadOutAngularAcceptance,
+                   Streams = [icetray.I3Frame.DAQ])
+
+tray.AddModule("I3Writer",
+               Filename = options.output_i3_file)
+tray.AddModule("TrashCan")
+
+if options.number_of_frames == 0:
+    tray.Execute()
+else:
+    tray.Execute(options.number_of_frames)
+
+tray.Finish()
+
+outfile = open(options.output_text_file, 'w')
+for number_of_dom_hits in numbers_of_dom_hits:
+  print >> outfile, number_of_dom_hits
+outfile.close()
+
+
+
 
