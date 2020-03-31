948d8b52ee39f9f3f0293ec1940fdeb42bf25563
Felix Schmuckermaier <felix.schmuckermaier@tum.de>
2020-03-31 08:06:14 -0500

    added folder structure

--- executable_POCAM.sh
+++ executable_POCAM.sh
@@ -6,7 +6,7 @@ eval $(/cvmfs/icecube.opensciencegrid.org/py3-v4.0.1/setup.sh)
 #Data stored at:
 path='/data/user/fschmuckermaier/data_raw/clsim_test'
 
-/home/fschmuckermaier/IceCube2/build/env-shell.sh python /home/fschmuckermaier/POCAM_scripts/generate_and_simulate_POCAM.py --output-i3-file="${path}/test_output.i3"
+/home/fschmuckermaier/IceCube2/build/env-shell.sh python /home/fschmuckermaier/POCAM_scripts/scripts/generate_and_simulate_POCAM.py --output-i3-file="${path}/test_output.i3"
 
 #Create q frame with POCAM as light source:
 #/home/fschmuckermaier/IceCube2/build/env-shell.sh python /home/fschmuckermaier/POCAM_scripts/create_qframe_i3_file.py --output-file="${path}/qframe.i3"
