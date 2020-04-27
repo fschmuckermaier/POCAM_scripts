8da635f5ec276106f7dfb0d0250193bb991e4dcd
Felix Schmuckermaier <felix.schmuckermaier@tum.de>
2020-04-27 06:54:20 -0500

    Replaced I3SPRNGRandomService with I3GSLRandomService

--- scripts/propagate_POCAM_photons.py
+++ scripts/propagate_POCAM_photons.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Takes photons from qframe and propagates them. Saves results in output-i3-file. 
+# Takes photons from qframe and propagates them. Saves results in output-i3-file.
 
 from optparse import OptionParser
 import glob
@@ -20,34 +20,27 @@ from I3Tray import * # otherwise the C++ modules have the wrong signatures
 parser = OptionParser(usage = "Usage: python propagate_photons_with_clsim.py options infile1.i3 infile2.i3 ...")
 parser.add_option("--output-i3-file", help = "I3 File to write the numbers of dom hits for each run to, e.g. tmp/numbers_of_dom_hits.i3")
 parser.add_option("--number-of-frames", type = "int", default = 0)
-parser.add_option("--number-of-parallel-runs", type = "int", default=1)
+parser.add_option("--gcd-file", type = "str",default="/home/fschmuckermaier/gcd/GeoCalibDetectorStatus_IC86.55697_corrected_V2.i3.gz")
 
 (options, args) = parser.parse_args()
 
 input_files = args
-
-#if isinstance(input_files, basestring):
-#    input_files = eval(input_files)
+gcd_file=expandvars(options.gcd_file)
 
 
 tray = I3Tray()
 tray.AddModule("I3Reader",
                FilenameList = input_files
 )
-tray.AddService("I3SPRNGRandomServiceFactory",
-                Seed = 123456,
-                NStreams = 2,
-                StreamNum = 1)
-randomService = phys_services.I3SPRNGRandomService(seed = options.seed,
-                                                   nstreams = 10000,
-                                                   streamnum = 1)
+
+randomService = phys_services.I3GSLGRandomService(seed = options.seed)
 
 common_clsim_parameters = dict(
     PhotonSeriesName = "PropagatedPhotons",
     RandomService = randomService,
     IceModelLocation = expandvars("$I3_SRC/clsim/resources/ice/spice_mie"),
+    GCDFile = gcd_file,
     UnWeightedPhotons = True,
-    ParallelEvents = options.number_of_parallel_runs,
     UnWeightedPhotonsScalingFactor = 1.0,
     DOMOversizeFactor = 1.0,
     UnshadowedFraction = 1.0,
