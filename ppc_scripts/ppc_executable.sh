1f2bddfd3b7dfabafec36dcc47b29c350a0f2e5d
Felix Schmuckermaier <felix.schmuckermaier@tum.de>
2020-06-30 08:21:27 -0500

    fixed pocam keys

--- ppc_scripts/ppc_executable.sh
+++ ppc_scripts/ppc_executable.sh
@@ -8,35 +8,35 @@ metaproject='/home/fschmuckermaier/IceCube/meta-projects/combo/stable/build'
 file_path="/data/user/fschmuckermaier/data_raw/ppc_test"
 file_name="test"
 string=88
-dom=71
+dom=72
 n_runs=10
 
 #run:
 $metaproject/env-shell.sh python /home/fschmuckermaier/POCAM_scripts/ppc_scripts/simple_POCAM_simulation.py --output-i3-file="${file_path}/${file_name}" --number-of-runs=$n_runs --string=$string --dom=$dom
 
 
-#Choose flashing POCAMs:  (all true POCAM positions were shifted by 1 downwards (e.g. true position = 87,4), since only OMs can be flashed somehow...)
-# 87 3
-# 87 83
-# 88 1
-# 88 71
-# 89 1
-# 89 9
-# 89 12
-# 89 37
-# 89 106
-# 90 11
-# 90 13
-# 90 99
-# 91 14
-# 91 49
-# 92 5
-# 92 17
-# 92 27
-# 93 7
-# 93 16
-# 93 63
-# 93 112
+#Choose flashing POCAMs:
+# 87 4
+# 87 84
+# 88 2
+# 88 72
+# 89 2
+# 89 10
+# 89 13
+# 89 38
+# 89 107
+# 90 12
+# 90 14
+# 90 100
+# 91 15
+# 91 50
+# 92 6
+# 92 18
+# 92 28
+# 93 8
+# 93 17
+# 93 64
+# 93 113
 
 
 
